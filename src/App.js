3974c1d2156248d80328a951884c56324bad49b3
Youu9 <younes-c@hotmail.fr>
2023-06-30 12:20:12 +0200

    create MovieCard component & dynamic rendre

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from './search.svg'
 //bf146db8
 
 const API_URL="http://www.omdbapi.com?apikey=bf146db8";
 
-const movie1 = {
-  "Title": "Superman, Spiderman or Batman",
-  "Year": "2011",
-  "imdbID": "tt2084949",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
-
 const App = () => {
-
+  const [movies, setMovies] = useState([]);
+  
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -43,20 +36,22 @@ const App = () => {
           onClick={() => {}}
         />
       </div>
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}></img>
-          </div>
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
-        </div>
-      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie}></MovieCard>
+              ))}
+            </div>
+          ) : 
+          (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>
   );
 }
